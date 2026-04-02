9205150c1a1e8277ec8fdc9e47a0d2ba550eafa9
Anthony Elliott <anthony.elliott@fiveq.com>
2026-04-02 15:20:22 -0500

    Delete old React Native code

--- rn-mip-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Use ts-jest for TypeScript support without React Native baggage
-  transform: {
-    '^.+\\.tsx?$': ['babel-jest', { presets: ['@babel/preset-typescript'] }],
-  },
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
